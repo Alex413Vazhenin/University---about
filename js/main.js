fcb2b225bbfdf6aa4dc29fe30f86d5cbcb5f13e6
Alexander <insaneall1@gmail.com>
2019-01-27 15:42:12 +0400

    Working on programs and courses tabs

---
+++ js/main.js
@@ -0,0 +1,13 @@
+	/*-----------------------------------------------------*/
+	/* tabs
+  	-------------------------------------------------------*/	
+	$(".tab-content").hide();
+	$(".tab-content").first().show();
+
+	$("ul.tabs li").click(function () {
+	   $("ul.tabs li").removeClass("active");
+	   $(this).addClass("active");
+	   $(".tab-content").hide();
+	   var activeTab = $(this).attr("data-id");
+	   $("#" + activeTab).fadeIn(700);
+	});
